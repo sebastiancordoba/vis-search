b9d746d28b87a8c19267da0260e26d85cdad318e
Emilio Ramirez <73008381+many1026@users.noreply.github.com>
2023-05-09 16:08:46 -0600

    bug bfs

--- BFS.js
+++ BFS.js
@@ -7,86 +7,87 @@
 //Además, utiliza un tiempo de espera de 1 segundo para que el proceso de búsqueda sea más visible en la pantalla.
 
 function bfs(startNode, targetNode) {
-    let queue = [];
-    queue.push(startNode);
-  
-    let visited = new Set();
-    visited.add(startNode);
-  
-    let parentMap = new Map();
-    parentMap.set(startNode, null);
-  
-    let delay = 0; // tiempo de espera de 1 segundo
-    let currentNode;
-  
-    function visitNextNode() {
-      if (queue.length > 0) {
-        currentNode = queue.shift();
-        if (currentNode === targetNode) {
-          // Se encontró el nodo objetivo
-          let path = [currentNode];
-          let parent = parentMap.get(currentNode);
-          while (parent !== null) {
-            path.unshift(parent);
-            parent = parentMap.get(parent);
-          }
-          console.log("Camino encontrado: ", path);
-          final_path = path;
-          start.visited = false;
-          start.color = color(0, 255, 0);
-          end.color = color(255, 0, 0);
-          return;
+  targetNode.visited = false;
+  let queue = [];
+  queue.push(startNode);
+
+  let visited = new Set();
+  visited.add(startNode);
+
+  let parentMap = new Map();
+  parentMap.set(startNode, null);
+
+  let delay = 0; // tiempo de espera de 1 segundo
+  let currentNode;
+
+  function visitNextNode() {
+    if (queue.length > 0) {
+      currentNode = queue.shift();
+      if (currentNode === targetNode) {
+        // Se encontró el nodo objetivo
+        let path = [currentNode];
+        let parent = parentMap.get(currentNode);
+        while (parent !== null) {
+          path.unshift(parent);
+          parent = parentMap.get(parent);
         }
-        currentNode.color = color(200, 255, 170);
+        console.log("Camino encontrado: ", path);
+        final_path = path;
+        start.visited = false;
         start.color = color(0, 255, 0);
-        let neighbors = getNeighbors_bfs(currentNode);
-        for (let neighbor of neighbors) {
-          if (!visited.has(neighbor)) {
-            visited.add(neighbor);
-            queue.push(neighbor);
-            parentMap.set(neighbor, currentNode);
-            neighbor.color = color(185, 250, 255);
-          }
-        }
-        setTimeout(visitNextNode, delay);
-      } else {
-        console.log("Nodo objetivo no encontrado.");
+        end.color = color(255, 0, 0);
+        return;
       }
-    }
-  
-    setTimeout(visitNextNode, delay);
-  }
-
-
-  function getNeighbors_bfs(node) {
-    const rows = grid.length;
-    const cols = grid[0].length;
-    const x = node.y / nodeSize;
-    const y = node.x / nodeSize;
-    const neighbors = [];
-  
-    // Check horizontal and vertical neighbors
-    if (x > 0) neighbors.push(grid[x - 1][y]);
-    if (x < rows - 1) neighbors.push(grid[x + 1][y]);
-    if (y > 0) neighbors.push(grid[x][y - 1]);
-    if (y < cols - 1) neighbors.push(grid[x][y + 1]);
-  
-    // Check diagonal neighbors with a penalty
-    if (x > 0 && y > 0) neighbors.push(grid[x - 1][y - 1]);
-    if (x > 0 && y < cols - 1) neighbors.push(grid[x - 1][y + 1]);
-    if (x < rows - 1 && y > 0) neighbors.push(grid[x + 1][y - 1]);
-    if (x < rows - 1 && y < cols - 1) neighbors.push(grid[x + 1][y + 1]);
-  
-    // Apply diagonal penalty
-    for (const neighbor of neighbors) {
-      if (neighbor != null) {
-        const dx = Math.abs(neighbor.x - x);
-        const dy = Math.abs(neighbor.y - y);
-        if (dx === 1 && dy === 1) {
-          neighbor.cost *= Math.sqrt(2); // Apply diagonal penalty
+      currentNode.color = color(200, 255, 170);
+      start.color = color(0, 255, 0);
+      let neighbors = getNeighbors_bfs(currentNode);
+      for (let neighbor of neighbors) {
+        if (!visited.has(neighbor)) {
+          visited.add(neighbor);
+          queue.push(neighbor);
+          parentMap.set(neighbor, currentNode);
+          neighbor.color = color(185, 250, 255);
         }
       }
+      setTimeout(visitNextNode, delay);
+    } else {
+      console.log("Nodo objetivo no encontrado.");
     }
-  
-    return neighbors.filter((neighbor) => neighbor != null && !neighbor.visited);
   }
+
+  setTimeout(visitNextNode, delay);
+}
+
+
+function getNeighbors_bfs(node) {
+  const rows = grid.length;
+  const cols = grid[0].length;
+  const x = node.y / nodeSize;
+  const y = node.x / nodeSize;
+  const neighbors = [];
+
+  // Check horizontal and vertical neighbors
+  if (x > 0) neighbors.push(grid[x - 1][y]);
+  if (x < rows - 1) neighbors.push(grid[x + 1][y]);
+  if (y > 0) neighbors.push(grid[x][y - 1]);
+  if (y < cols - 1) neighbors.push(grid[x][y + 1]);
+
+  // Check diagonal neighbors with a penalty
+  if (x > 0 && y > 0) neighbors.push(grid[x - 1][y - 1]);
+  if (x > 0 && y < cols - 1) neighbors.push(grid[x - 1][y + 1]);
+  if (x < rows - 1 && y > 0) neighbors.push(grid[x + 1][y - 1]);
+  if (x < rows - 1 && y < cols - 1) neighbors.push(grid[x + 1][y + 1]);
+
+  // Apply diagonal penalty
+  for (const neighbor of neighbors) {
+    if (neighbor != null) {
+      const dx = Math.abs(neighbor.x - x);
+      const dy = Math.abs(neighbor.y - y);
+      if (dx === 1 && dy === 1) {
+        neighbor.cost *= Math.sqrt(2); // Apply diagonal penalty
+      }
+    }
+  }
+
+  return neighbors.filter((neighbor) => neighbor != null && !neighbor.visited);
+}
